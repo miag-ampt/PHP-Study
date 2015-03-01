e45b1101b5a895ab90c6d5181d477d290a96cf0e
miag-ampt <Sortie-Ice@Sortie-Ice.local>
2015-03-02 00:11:23 +0900

    7章:フレームワークによる効率的な開発 7.2:フレームワークの作成 まで終了いたしました。

--- application/core/Request.php
+++ application/core/Request.php
@@ -1,84 +1,88 @@
 <?php
 
+
 class Request
 {
-	public function isPost()
-	{
-		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-			return ture;
-		}
+   
+    public function isPost()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	public function getGet($name, $default = null)
-	{
-		if (isset($_GET[$name])) {
-			return $_GET[$name];
-		}
+    
+    public function getGet($name, $default = null)
+    {
+        if (isset($_GET[$name])) {
+            return $_GET[$name];
+        }
 
-		return $default;
-	}
+        return $default;
+    }
 
-	public function getPost($name, $default = null)
-	{
-		if (isset($_POST[$name])) {
-			return $_POST[$name];
-		}
+   
+    public function getPost($name, $default = null)
+    {
+        if (isset($_POST[$name])) {
+            return $_POST[$name];
+        }
 
-		return $default;
-	}
+        return $default;
+    }
 
-	public function getHost()
-	{
-		if (!empty($_SERVER['HTTP_HOST'])) {
-			return $_SERVER['HTTP_HOST'];
-		}
+    public function getHost()
+    {
+        if (!empty($_SERVER['HTTP_HOST'])) {
+            return $_SERVER['HTTP_HOST'];
+        }
+        
+        return $_SERVER['SERVER_NAME'];
+    }
 
-		return $_SERVER['SERVER_NAME'];
-	}
+  
+    public function isSsl()
+    {
+        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+            return true;
+        }
+        return false;
+    }
 
-	public function isSsl()
-	{
-		if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
-			return ture;
-		}
+  
+    public function getRequestUri()
+    {
+        return $_SERVER['REQUEST_URI'];
+    }
 
-		return false;
-	}
+    public function getBaseUrl()
+    {
+        $script_name = $_SERVER['SCRIPT_NAME'];
 
-	public function getRequestUri()
-	{
-		return $_SERVER['REQUEST_URI'];
-	}
+        $request_uri = $this->getRequestUri();
 
-	public function getBaseUrl()
-	{
-		$script_name = $_SERVER['SCRIPT_NAME'];
+        if (0 === strpos($request_uri, $script_name)) {
+            return $script_name;
+        } else if (0 === strpos($request_uri, dirname($script_name))) {
+            return rtrim(dirname($script_name), '/');
+        }
 
-		$request_url = $this->getRequestUri();
+        return '';
+    }
 
-		if (0 === strpos($request_uri, $script_name)) {
-			return $script_name;
-		} else if (0 === strpos($request_uri, dirname($script_name))) {
-			return rtrim(dirname($script_name), '/');
-		}
+    public function getPathInfo()
+    {
+        $base_url = $this->getBaseUrl();
+        $request_uri = $this->getRequestUri();
 
-		return '':
-	}
+        if (false !== ($pos = strpos($request_uri, '?'))) {
+            $request_uri = substr($request_uri, 0, $pos);
+        }
 
-	public function getPathInfo()
-	{
-		$base_url = $this->getBaseUrl();
-		$request_uri = $this->getRequestUri();
+        $path_info = (string)substr($request_uri, strlen($base_url));
 
-		if (false !== ($pos = strpos($request_uri, '?'))) {
-			$request_uri = substr($request_uri, 0, $pos);
-		}
-
-		$path_info = (string)substr($request_uri, strlen($base_url));
-
-		return $path_info;
-	}
-}
+        return $path_info;
+    }
 }
--- application/core/Router.php
+++ application/core/Router.php
@@ -1,51 +1,53 @@
 <?php
 
+
 class Router
 {
-	protected $routes;
+    protected $routes;
 
-	public function __construct($definitions)
-	{
-		$this->routes = $this->compileRoutes($difinitions);
-	}
+   
+    public function __construct($definitions)
+    {
+        $this->routes = $this->compileRoutes($definitions);
+    }
 
-	public function compileRoutes($difinitions)
-	{
-		$routes = array();
+  
+    public function compileRoutes($definitions)
+    {
+        $routes = array();
 
-		foreach ($definitions as $url => $params) {
-			$tokens = explode('/', ltrim($url, '/'));
-			foreach ($tokens as $i => $token) {
-				if (0 === strpos($token, ':')) {
-					$name = substr($token, 1);
-					$token = '(?P<' . $name . '>[^/]+)';
-				}
-				$tokens[$i] = $token;
-			}
+        foreach ($definitions as $url => $params) {
+            $tokens = explode('/', ltrim($url, '/'));
+            foreach ($tokens as $i => $token) {
+                if (0 === strpos($token, ':')) {
+                    $name = substr($token, 1);
+                    $token = '(?P<' . $name . '>[^/]+)';
+                }
+                $tokens[$i] = $token;
+            }
 
-			$pattern = '/' . implode('/', $tokens);
-			$routes[$pattern] = $params;
-		}
+            $pattern = '/' . implode('/', $tokens);
+            $routes[$pattern] = $params;
+        }
 
-		return $routes;
-	}
+        return $routes;
+    }
 
-	public function resolve($path_info)
-	{
-		if ('/' !== substr($path_info, 0, 1)) {
-			$path_info ='/' . $path_info;
-		}
+  
+    public function resolve($path_info)
+    {
+        if ('/' !== substr($path_info, 0, 1)) {
+            $path_info = '/' . $path_info;
+        }
 
-		foreach ($this->routers as $pattern => $params) {
-			if (preg_match('#^' . $pattern . '$#', $path_info, $matches)) {
-				$params = array_merge($params, $matches);
+        foreach ($this->routes as $pattern => $params) {
+            if (preg_match('#^' . $pattern . '$#', $path_info, $matches)) {
+                $params = array_merge($params, $matches); 
 
-				return $params;
-			}
-		}
+                return $params;
+            }
+        }
 
-		return false;
-	}
-
+        return false;
+    }
 }
-
